48ef18bb50dea8efdcd16e0038de58f45a0b1450
Junho <junhokim@sejongtelecom.net>
2021-12-27 11:44:40 +0900

    fixed jshint es6 grammer

--- chapter8/tree-diameter.js
+++ chapter8/tree-diameter.js
@@ -1,27 +1,30 @@
 let tree = [0, [2, 3, 4], [5, 6], 0, [7], 0, 0, 0];
-
-console.log(diameter(1));
+let TREE_CHILD_BROTHER = 0;
+let TREE_CHILD_SISTER = 1;
 
 function diameter(root) {
     let arr = tree[root];
 
-    let [a, b, ...c] = arr
-        .map((val, idx) => {
+    let [a, b, ...rest] = arr
+        .map((val) => {
             return idxSum(val, 1);
         })
         .sort((a, b) => b - a);
 
     return a + b;
-}
 
-function idxSum(idx, sum) {
-    if (tree[idx] == 0) {
-        return sum;
-    } else {
-        if (tree[idx].length > 1) {
-            return Math.max(idxSum(tree[idx][0], sum + 1), idxSum(tree[idx][1], sum + 1));
+    function idxSum(idx, sum) {
+        if (tree[idx] === 0) {
+            return sum;
         } else {
-            return idxSum(tree[idx][0], sum + 1);
+            if (tree[idx].length > 1) {
+                return Math.max(idxSum(tree[idx][TREE_CHILD_BROTHER], sum + 1), idxSum(tree[idx][TREE_CHILD_SISTER], sum + 1));
+            } else {
+                return idxSum(tree[idx][0], sum + 1);
+            }
         }
     }
 }
+
+let result = diameter(1);
+console.log(result);
